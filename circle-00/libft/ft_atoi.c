262da2588742e1240f40b50bae226ea13529e26e
Isaji23 <isjm2323@gmail.com>
2025-10-30 22:42:25 +0100

    ft_atoi y ft_calloc

---
+++ circle-00/libft/ft_atoi.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+int		ft_atoi(const char *nptr)
+{
+	long result = 0;
+	int sign = 1;
+
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '-')
+	{
+		sign = -1;
+		nptr++;
+	}
+	else if (*nptr == '+')
+		nptr++;
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		result = result * 10 + (*nptr - '0');
+		nptr++;
+	}
+	return (result * sign);
+}
---
+++ circle-00/libft/ft_calloc.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*result;
+
+	if (nmemb == 0 || size == 0)
+		return malloc(0);
+	if (nmemb > __SIZE_MAX__ / size)
+		return (NULL);
+	result = malloc(nmemb * size);
+	if (!result)
+		return (NULL);
+	ft_bzero(result, nmemb * size);
+	return (result);
+}
